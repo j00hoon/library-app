67508e264f02bb91442778deced457f0f7b2a711
Seunghoon Baik <j00hoon1101@gmail.com>
2023-11-16 08:13:42 -0500

    Return & renew book is updated 11/16/2023

--- backend/spring-boot-library-app/src/main/java/com/baik/libraryapp/controller/BookController.java
+++ backend/spring-boot-library-app/src/main/java/com/baik/libraryapp/controller/BookController.java
@@ -1,10 +1,13 @@
 package com.baik.libraryapp.controller;
 
 import com.baik.libraryapp.entity.Book;
+import com.baik.libraryapp.responsemodels.ShelfCurrentLoansResponse;
 import com.baik.libraryapp.service.BookService;
 import com.baik.libraryapp.utils.ExtractJWT;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin("http://localhost:3000")
 @RestController
@@ -16,6 +19,16 @@ public class BookController {
     @Autowired
     public BookController(BookService bookService) {
         this.bookService = bookService;
+    }
+
+
+
+    @GetMapping("/secure/currentloans")
+    public List<ShelfCurrentLoansResponse> currentLoans(@RequestHeader(value = "Authorization") String token)
+            throws Exception {
+
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        return bookService.currentLoans(userEmail);
     }
 
 
@@ -43,6 +56,20 @@ public class BookController {
     }
 
 
+    @PutMapping("/secure/return")
+    public void returnBook (@RequestHeader(value = "Authorization") String token,
+                            @RequestParam Long bookId) throws Exception {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        bookService.returnBook(userEmail, bookId);
+    }
+
+
+    @PutMapping("/secure/renew/loan")
+    public void renewLoan (@RequestHeader(value = "Authorization") String token,
+                           @RequestParam Long bookId) throws Exception {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        bookService.renewLoan(userEmail, bookId);
+    }
 
 
 
